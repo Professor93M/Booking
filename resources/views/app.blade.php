91404601857e50f0026d7d41adf6215c88c3e89c
watan <drmody007.d0@gmail.com>
2022-01-03 00:09:31 +0300

    added dashboard

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
-<html lang="ar" dir="rtl" >
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="rtl">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <title inertia>{{ config('app.name', 'Snack Time') }}</title>
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
@@ -17,11 +14,12 @@
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
+    
     <body class="font-tajawal-bold antialiased">
         @inertia
 
         @env ('local')
-            <script src="http://localhost:8080/js/bundle.js"></script>
+            {{-- <script src="http://localhost:8080/js/bundle.js"></script> --}}
         @endenv
     </body>
 </html>
