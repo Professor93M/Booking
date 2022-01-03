e6cdbbe414dcfab1141980b6dc44fe6ed1693cc9
Professor93M <professor404@gmail.com>
2022-01-03 12:57:02 +0300

    fix route error

--- routes/auth.php
+++ routes/auth.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\Controller;
 use App\Http\Controllers\MenuController;
 use Illuminate\Support\Facades\Route;
 
